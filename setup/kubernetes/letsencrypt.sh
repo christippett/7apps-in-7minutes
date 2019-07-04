59c2fffe4a0f718a1ce897cc73b6111728e5d5e4
Chris Tippett <c.tippett@gmail.com>
2019-07-04 22:39:25 +1000

    Update script to provision SSL certificate for Cloud Run on GKE

--- setup/kubernetes/letsencrypt.sh
+++ setup/kubernetes/letsencrypt.sh
@@ -1,10 +1,6 @@
+#!/bin/sh
+# Create manual SSL certificate for Cloud Run on GKE
 gcloud container clusters get-credentials demo-cluster --zone australia-southeast1-b --project servian-app-demo
-
 kubectl create --namespace istio-system secret tls istio-ingressgateway-certs \
     --key privkey.pem \
     --cert fullchain.pem
-
-
-kubectl create clusterrolebinding chris-admin \
-    --clusterrole=cluster-admin \
-    --user=chris.tippett@servian.com
