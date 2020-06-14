c712b19477c10ac9f6008566167b7154485be945
Chris Tippett <c.tippett@gmail.com>
2020-06-14 15:09:27 +1000

    2020-revisit: restructure setup folder

--- setup/envsubst/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mv $1 $1.bak
-envsubst < $1.bak > $1
--- setup/terraform/network.tf
+++
@@ -1,8 +0,0 @@
-data "google_compute_network" "default" {
-  name = "default"
-}
-
-data "google_compute_subnetwork" "default" {
-  name   = "default"
-  region = "australia-southeast1"
-}
