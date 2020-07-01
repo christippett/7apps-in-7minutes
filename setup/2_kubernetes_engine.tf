e5f8a083f9d01eacb215db1101b15769673d4886
Chris Tippett <c.tippett@gmail.com>
2020-07-02 03:23:09 +1000

    refactor: Re-configure GKE

--- setup/2_kubernetes_engine.tf
+++ setup/2_kubernetes_engine.tf
@@ -7,11 +7,12 @@
 resource "google_container_cluster" "gke" {
   provider = google-beta
 
-  name       = "gke-7apps"
-  location   = "${var.region}-a"
-  project    = var.project_id
-  subnetwork = google_compute_subnetwork.default.name
-  network    = google_compute_network.default.name
+  name               = "gke-7apps"
+  min_master_version = "1.16.8-gke.15"
+  location           = "${var.region}-a"
+  project            = var.project_id
+  subnetwork         = google_compute_subnetwork.default.name
+  network            = google_compute_network.default.name
 
   remove_default_node_pool = true
   initial_node_count       = 1 # will be replaced immediately upon cluster creation
@@ -19,6 +20,10 @@ resource "google_container_cluster" "gke" {
   ip_allocation_policy {
     cluster_ipv4_cidr_block  = "172.16.0.0/17"   # 172.16.1.0 - 172.16.127.255
     services_ipv4_cidr_block = "172.16.128.0/17" # 172.16.128.0 - 172.16.255.255
+  }
+
+  workload_identity_config {
+    identity_namespace = "${var.project_id}.svc.id.goog"
   }
 
   addons_config {
@@ -29,7 +34,9 @@ resource "google_container_cluster" "gke" {
     http_load_balancing {
       disabled = false
     }
-
+    istio_config {
+      disabled = false
+    }
     cloudrun_config {
       disabled = false
     }
@@ -55,22 +62,59 @@ resource "google_container_node_pool" "preemptible" {
   location           = google_container_cluster.gke.location
   cluster            = google_container_cluster.gke.name
   initial_node_count = 3
-  max_pods_per_node  = 50
+  version            = "1.16.8-gke.15"
 
   node_config {
-    preemptible     = true
-    machine_type    = "e2-standard-2"
-    metadata        = { disable-legacy-endpoints = "true" }
-    service_account = google_service_account.default.email
-    oauth_scopes    = ["https://www.googleapis.com/auth/cloud-platform"]
+    preemptible  = true
+    machine_type = "n1-standard-1"
+    metadata     = { disable-legacy-endpoints = "true" }
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
 }
+
+/* IAM ---------------------------------------------------------------------- */
+
+data "google_compute_default_service_account" "default" {}
+
+resource "google_project_iam_member" "compute_default" {
+  for_each = toset([
+    "roles/gkehub.connect",
+    "roles/container.hostServiceAgentUser",
+    "roles/monitoring.metricWriter",
+    "roles/logging.logWriter"
+  ])
+
+  project = var.project_id
+  role    = each.value
+  member  = "serviceAccount:${data.google_compute_default_service_account.default.email}"
+}
+
+resource "kubernetes_cluster_role_binding" "gke" {
+  metadata {
+    name = "admins"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "User"
+    name      = data.google_compute_default_service_account.default.email
+    api_group = "rbac.authorization.k8s.io"
+  }
+  subject {
+    kind      = "User"
+    name      = var.email
+    namespace = "rbac.authorization.k8s.io"
+  }
+}
+
+# /* DNS ---------------------------------------------------------------------- */
 
 resource "google_compute_global_address" "gke_https_lb" {
   name = "ip-https-lb-gke"
 }
-
-/* DNS ---------------------------------------------------------------------- */
 
 resource "google_dns_record_set" "gke" {
   name         = "${var.kubernetes_engine_subdomain}.${var.domain_name}."
@@ -78,4 +122,50 @@ resource "google_dns_record_set" "gke" {
   ttl          = 300
   managed_zone = google_dns_managed_zone.dns.name
   rrdatas      = [google_compute_global_address.gke_https_lb.address]
+}
+
+/* ========================================================================== */
+/*                       Caddy Server Ingress Controller                      */
+/* ========================================================================== */
+
+# Automatic SSL/TLS certificates through Let's Encrypt
+# https://github.com/caddyserver/ingress
+
+
+resource "kubernetes_namespace" "caddy" {
+  metadata {
+    name = "caddy-system"
+  }
+  depends_on = [google_container_cluster.gke]
+}
+
+resource "helm_release" "caddy" {
+  name       = "caddy-ingress"
+  namespace  = "caddy-system"
+  repository = "https://caddyserver.github.io/ingress/"
+  chart      = "caddy-ingress-controller"
+  atomic     = true
+
+  set {
+    name  = "image.tag"
+    value = "latest"
+  }
+
+  set {
+    name  = "ingressController.autotls"
+    value = true
+  }
+
+  set {
+    name  = "ingressController.email"
+    value = var.email
+  }
+
+
+  set {
+    name  = "service.loadBalancerIP"
+    value = google_compute_global_address.gke_https_lb.address
+  }
+
+  depends_on = [google_container_cluster.gke]
 }
