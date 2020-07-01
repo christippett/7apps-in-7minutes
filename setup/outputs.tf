ae1d8cb448ad65dbbf5079a5aa7ee9f696964b50
Chris Tippett <c.tippett@gmail.com>
2020-07-02 00:23:01 +1000

    style: Rename outputs (some more)

--- setup/outputs.tf
+++ setup/outputs.tf
@@ -4,12 +4,13 @@
 
 /* DNS ---------------------------------------------------------------------- */
 
-output "dns_nameservers" {
-  value = google_dns_managed_zone.dns.name_servers
-}
-
-output "app_nameservers" {
+output "app_dns_records" {
   value = {
+    root = {
+      name   = var.domain
+      type   = "NS"
+      rrdata = google_dns_managed_zone.dns.name_servers
+    }
     cloud_run_managed  = google_cloud_run_domain_mapping.managed.status[0].resource_records
     cloud_run_anthos   = google_cloud_run_domain_mapping.anthos.status[0].resource_records
     appengine_standard = google_app_engine_domain_mapping.standard.resource_records
