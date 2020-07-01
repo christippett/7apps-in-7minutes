5b6fa107a4465ff7a85a2cb9dce110e93f647db5
Chris Tippett <c.tippett@gmail.com>
2020-07-02 04:04:31 +1000

    fix: Remove Cloud SQL from outputs

--- setup/outputs.tf
+++ setup/outputs.tf
@@ -35,20 +35,3 @@ output "app_dns_records" {
     }
   }
 }
-
-
-/* Cloud SQL ---------------------------------------------------------------- */
-
-output "db_connection_parameters" {
-  value = {
-    host = [
-      module.cloudsql.private_ip_address,
-      module.cloudsql.public_ip_address
-    ]
-    port        = "5432"
-    user        = local.db_user
-    password    = local.db_password
-    public_url  = "postgres://${local.db_user}:${local.db_password}@${module.cloudsql.public_ip_address}:5432/${local.db_name}"
-    private_url = "postgres://${local.db_user}:${local.db_password}@${module.cloudsql.private_ip_address}:5432/${local.db_name}"
-  }
-}
