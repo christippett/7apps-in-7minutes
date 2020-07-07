6a8d668afecffb43f13438f9e75611a55df9ed27
Chris Tippett <c.tippett@gmail.com>
2020-07-07 14:42:06 +1000

    feat: Return git commit on localhost

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import os
 from random import randrange
 
-from flask import Flask, Request, render_template, request
+from flask import Flask, Request, jsonify, render_template, request
+from flask_cors import CORS
 from pyfiglet import Figlet
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "Localhost")
@@ -31,13 +32,23 @@ ASCII_FONT = ASCII_OPTIONS[randrange(0, len(ASCII_OPTIONS) - 1)]
 try:
     COMMIT_SHA = open("commit_sha.txt", "r").read()
 except FileNotFoundError:
-    COMMIT_SHA = "*****"
+    import git
+
+    repo = git.Repo(search_parent_directories=True)
+    COMMIT_SHA = repo.head.object.hexsha[:7]
 
 app = Flask("7apps")
+CORS(app)
+
 fmt = Figlet(font=ASCII_FONT)
 
 
 def main(req: Request):
+    accept_language = req.headers.get("Accept-Language")
+    if accept_language == "application/json":
+        resp = jsonify(commit_sha=COMMIT_SHA)
+        resp.headers.add_header("Access-Control-Allow-Origin", "*")
+        return resp
     title = req.args.get("title", "7Apps")
     ascii_title = fmt.renderText(title)
     return render_template(
