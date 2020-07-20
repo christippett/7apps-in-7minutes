01d783a0f293faec54026a483102cf6e8adbc13a
Chris Tippett <c.tippett@gmail.com>
2020-07-21 08:13:22 +1000

    feat: Update app styles

--- src/app/main.py
+++ src/app/main.py
@@ -1,35 +1,15 @@
 import os
-from random import randrange
 
 from flask import Flask, jsonify, render_template, request
 from flask_cors import cross_origin
 from pyfiglet import Figlet
 
+from common import ascii_font, background_gradient, page_font
+
 # TODO: get gradient programmatically from https://raw.githubusercontent.com/ghosh/uiGradients/master/gradients.json
 
 APP_TITLE = os.environ.get("APP_TITLE", "Localhost")
-BG_CLASS = randrange(0, 10)
-FONT_CLASS = randrange(0, 6)
-ASCII_OPTIONS = [
-    "big",
-    "stop",
-    "rounded",
-    "cricket",
-    "contrast",
-    "banner",
-    "larry3d",
-    "ogre",
-    "speed",
-    "smkeyboard",
-    "graffiti",
-    "fuzzy",
-    "lean",
-    "moscow",
-    "pawp",
-    "rectangles",
-    "serifcap",
-]
-ASCII_FONT = ASCII_OPTIONS[randrange(0, len(ASCII_OPTIONS) - 1)]
+
 
 try:
     COMMIT_SHA = open("commit_sha.txt", "r").read()
@@ -41,7 +21,7 @@ except FileNotFoundError:
 
 app = Flask("7apps")
 
-fmt = Figlet(font=ASCII_FONT)
+fmt = Figlet(font=ascii_font)
 
 
 @app.route("/")
@@ -57,12 +37,12 @@ def main(*args, **kwargs):
     return render_template(
         "index.html",
         host=request.base_url.replace("http://", "https://"),
-        service_name=APP_TITLE,
+        title=APP_TITLE,
         ascii_header=ascii_header,
-        ascii_font=ASCII_FONT,
+        ascii_font=ascii_font,
+        page_font=page_font,
+        background_gradient=background_gradient,
         commit_sha=COMMIT_SHA.strip(),
-        bg_class=BG_CLASS,
-        font_class=FONT_CLASS,
     )
 
 
