10edd8823fe963f6d486fddda6a9fc578e67165a
Chris Tippett <c.tippett@gmail.com>
2020-08-14 05:29:48 +1000

    demo: feat: Use Cloud Scheduler to reset app

--- terraform/setup/cloud_build/main.tf
+++ terraform/setup/cloud_build/main.tf
@@ -17,6 +17,7 @@ resource "google_cloudbuild_trigger" "deploy" {
   provider = google-beta
   project  = var.project_id
   name     = "7-APPS-DEPLOYMENT"
+  disabled = true
 
   ignored_files = ["dashboard/**", "docs/**", "terraform/**"]
   filename      = "app/cloudbuild.yaml"
@@ -30,9 +31,58 @@ resource "google_cloudbuild_trigger" "deploy" {
   }
 }
 
+/* Cloud Scheduler ---------------------------------------------------------- */
+
+# Reset the app every half-hour
+
+resource "google_cloud_scheduler_job" "reset" {
+  project     = var.project_id
+  region      = var.region
+  name        = "reset-app"
+  description = "Reset application"
+  schedule    = "*/30 * * * *"
+
+  time_zone        = "Australia/Melbourne"
+  attempt_deadline = "660s"
+
+  http_target {
+    http_method = "POST"
+
+    uri = "https://cloudbuild.googleapis.com/v1/${google_cloudbuild_trigger.deploy.id}:run"
+    body = base64encode(jsonencode({
+      repoName   = var.github_branch,
+      branchName = var.github_repo
+    }))
+
+    headers = {
+      Content-Type = "application/json"
+    }
+
+    oauth_token {
+      service_account_email = google_service_account.scheduler.email
+      scope                 = "https://www.googleapis.com/auth/cloud-platform"
+    }
+  }
+}
+
 /* IAM ---------------------------------------------------------------------- */
 
-# Give Cloud Build perimssion to deploy... and pretty much anything else 🤪
+# Create a service account for Cloud Scheduler
+
+resource "google_service_account" "scheduler" {
+  account_id   = "cloud-scheduler"
+  display_name = "🤖 Cloud Scheduler service account"
+  description  = "🕹️️️ Account for running Cloud Build jobs"
+  project      = var.project_id
+}
+
+resource "google_project_iam_member" "scheduler" {
+  project = var.project_id
+  role    = "roles/cloudbuild.builds.editor"
+  member  = "serviceAccount:${google_service_account.scheduler.email}"
+}
+
+# Give Cloud Build god mode over the project
 
 resource "google_project_iam_member" "project" {
   for_each = toset(["roles/editor", "roles/iap.tunnelResourceAccessor"])
--- terraform/setup/cloud_build/outputs.tf
+++ terraform/setup/cloud_build/outputs.tf
@@ -1,7 +1,3 @@
 output "cloud_build_trigger_id" {
   value = google_cloudbuild_trigger.deploy.id
 }
-
-output "cloud_build_topic" {
-  value = google_pubsub_topic.logs
-}
