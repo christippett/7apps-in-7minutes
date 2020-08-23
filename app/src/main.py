48138a3baa4a38182f234343259fd123f427f4b3
Chris Tippett <c.tippett@gmail.com>
2020-08-23 20:47:04 +1000

    demo: refactor: Tidy up demo app

--- app/src/main.py
+++ app/src/main.py
@@ -5,97 +5,99 @@ import random
 from dataclasses import dataclass
 from typing import List, Optional
 
-from colour import Color
 from flask import Flask, jsonify, render_template, request
 from flask_cors import cross_origin
-from pyfiglet import Figlet
+from pyfiglet import FigletFont
+from pyfiglet import figlet_format as fmt
 
-VERSION = os.getenv("VERSION")
-FONT = os.getenv("FONT")
-ASCII_FONT = os.getenv("ASCII_FONT")
-GRADIENT = os.getenv("GRADIENT")
+with open("theme-options.json", "r") as f:
+    THEME_OPTIONS = json.load(f)
+
+
+def get_random(options):
+    return random.choices(options, k=1)[0]
 
 
 @dataclass
-class AppTheme:
-    font: Optional[str] = None
-    ascii_font: Optional[str] = None
-    gradient: Optional[str] = None
-    colors: List[str] = dataclasses.field(init=False)
+class Theme:
+    font: Optional[str] = os.getenv("FONT")
+    ascii_font: Optional[str] = os.getenv("ASCII_FONT")
+    gradient: Optional[str] = os.getenv("GRADIENT")
 
     def __post_init__(self):
-        with open("theme.json", "r") as f:
-            theme = json.load(f)
-        self.gradients = {
-            g["name"]: g["colors"]
-            for g in theme["gradients"]
-            if self.is_vibrant(list(map(Color, g["colors"])))
-        }
-        if not self.gradient:
-            self.gradient = random.choice(list(self.gradients.keys()))
-        if not self.font:
-            self.font = random.choice(theme["fonts"])
-        if not self.ascii_font:
-            self.ascii_font = random.choice(theme["ascii_fonts"])
-        self.colors = self.gradients[self.gradient]
+        # Silently replace any unavailable theme properties with something
+        # random instead of raising an exception
+        if self.font is None:
+            self.font = self.get_random_font()
+        if self.ascii_font not in FigletFont.getFonts():
+            self.ascii_font = self.get_random_ascii_font()
+        if self.gradient is None or len(self.colors) == 0:
+            self.gradient = self.get_random_gradient()
 
-    def is_vibrant(self, colors: List[Color]):
-        return (
-            min([c.get_saturation() for c in colors]) > 0.8
-            and min([c.get_luminance() for c in colors]) < 0.5
-        )
+    @property
+    def colors(self) -> List[str]:
+        gradient = [g for g in THEME_OPTIONS["gradients"] if g["name"] == self.gradient]
+        return gradient[0]["colors"] if gradient else []
+
+    def get_random_font(self):
+        return get_random(THEME_OPTIONS["fonts"])
+
+    def get_random_ascii_font(self):
+        fonts = FigletFont.getFonts()
+        fitted = [f for f in fonts if len(fmt("7Apps", f).split("\n")) <= 15]
+        return get_random(fitted)
+
+    def get_random_gradient(self):
+        return get_random(THEME_OPTIONS["gradients"])["name"]
 
 
 @dataclass
 class App:
-    title: str
-    version: Optional[str]
-    theme: AppTheme
+    id: str
+    theme: Theme
+    title: Optional[str] = "7-Apps in 7-Minutes"
+    version: Optional[str] = os.getenv("VERSION", "unknown")
 
     @property
     def header(self) -> str:
-        fmt = Figlet(font=self.theme.ascii_font, direction="left-to-right")
-        return fmt.renderText("7Apps")
+        return fmt("7Apps", font=self.theme.ascii_font)
 
+    @classmethod
+    def from_env(cls, **kwargs):
+        if os.getenv("GAE_ENV") == "standard":
+            id, title = "standard", "App Engine: Standard"
+        elif os.getenv("GAE_SERVICE") == "flexible":
+            id, title = "flex", "App Engine: Flexible"
+        elif "FUNCTION_TARGET" in os.environ:
+            id, title = "function", "Cloud Function"
+        elif "K_SERVICE" in os.environ and any("ANTHOS" in v for v in os.environ):
+            id, title = "run-anthos", "Cloud Run: Anthos"
+        elif "K_SERVICE" in os.environ:
+            id, title = "run", "Cloud Run"
+        elif any(v.startswith("GKE_APP") for v in os.environ):
+            id, title = "kubernetes", "Kubernetes"
+        elif "GCE_APP" in os.environ:
+            id, title = "compute-engine", "Compute Engine"
+        else:
+            id = "other"
+            title, _, _ = request.host.rpartition(":")
+        return cls(id=id, title=title.title(), **kwargs)
 
-# Set title based on inferred runtime environment
-if os.getenv("GAE_ENV") == "standard":
-    title = "App Engine: Standard"
-elif os.getenv("GAE_SERVICE") == "flexible":
-    title = "App Engine: Flexible"
-elif "FUNCTION_TARGET" in os.environ:
-    title = "Cloud Function"
-elif "K_SERVICE" in os.environ and any("ANTHOS" in v for v in os.environ):
-    title = "Cloud Run: Anthos"
-elif "K_SERVICE" in os.environ:
-    title = "Cloud Run"
-elif any(v.startswith("GKE_APP") for v in os.environ):
-    title = "Kubernetes"
-elif "GCE_APP" in os.environ:
-    title = "Compute Engine"
-else:
-    title = "Demo"
 
 app = Flask("7apps")
-theme = AppTheme(font=FONT, ascii_font=ASCII_FONT, gradient=GRADIENT)
+theme = Theme()
 
 
 @app.route("/")
 @cross_origin(send_wildcard=True)
 def main(*args, **kwargs):
-    app_data = App(title=title, version=VERSION, theme=theme)
-
-    # Override theme properties from request parameters
-    if request.args and all([a in AppTheme.__dataclass_fields__ for a in request.args]):
-        app_data.theme = AppTheme(**request.args)
-        app_data.title = "Preview"
-        app_data.version = None
+    app_info = App.from_env(theme=theme)
 
     # Return machine-readable app info (used by dashboard)
     if request.headers.get("Accept") == "application/json":
-        return jsonify(dataclasses.asdict(app_data))
+        return jsonify(dataclasses.asdict(app_info))
 
-    return render_template("index.html", app=app_data)
+    return render_template("index.html", app=app_info)
 
 
 if __name__ == "__main__":
