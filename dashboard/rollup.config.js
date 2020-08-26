c265a16cd38ea2cd5781fa0364076cc8be0e2913
Chris Tippett <c.tippett@gmail.com>
2020-08-27 05:54:37 +1000

    refactor: JS output

--- dashboard/rollup.config.js
+++ dashboard/rollup.config.js
@@ -2,9 +2,9 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 
 export default {
-  input: 'src/static/js/index.js',
+  input: 'src/static/js/main.js',
   output: {
-    file: 'src/static/js/bundle.js',
+    file: 'src/static/js/7apps-bundle.js',
     format: 'iife',
     globals: {
       window: 'window',
